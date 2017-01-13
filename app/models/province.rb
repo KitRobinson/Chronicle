1b56e2d5c0de5579e0d9ffecb3aa5d51af4f6020
Kit Robinson <krobinson.dev@gmail.com>
2017-01-13 00:24:32 -0600

    add basic output methods to Province for console testing

--- app/models/province.rb
+++ app/models/province.rb
@@ -9,4 +9,43 @@ class Province < ActiveRecord::Base
 	belongs_to :primary_terrain, class_name: "Terrain"
 	belongs_to :secondary_terrain, class_name: "Terrain"
 	belongs_to :region
+
+	def get_adjacents
+		#show neighbors should return an array of neighboring provinces regardless of which is the 'province' and which the 'neighbor'
+		self.neighbors + others_neighbors
+	end
+
+	def get_development
+		#return a value for the provinces development based on:
+		base_dev * primary_terrain.development_multiplier * ((secondary_terrain.development_multiplier + 1)/2) + pop_dev
+	end
+
+	def pop_dev
+		return 0
+	end
+
+	def promote_secondary_terrain
+		start_primary = primary_terrain
+		primary_terrain = secondary_terrain
+		secondary_terrain = start_primary	
+	end
+
+	def push_primary_terrain(terrain)
+		secondary_terrain = primary_terrain
+		primary_terrain = terrain
+	end
+
+	def to_s
+		descriptor = ""
+		descriptor += "name: #{name}\n"
+		descriptor += "region: #{region.name}\n"
+		descriptor += "Primary Terrain: #{primary_terrain.name}, Secondary Terrain #{secondary_terrain.name}\n"
+		descriptor += "Base Development: #{base_dev}, Population Development: #{pop_dev}, Total Development: #{get_development}\n"
+		descriptor += "Borders: "
+		get_adjacents.each do |adj|
+			descriptor += adj.name
+			descriptor += " "
+		end
+		descriptor += "\n"
+	end
 end
