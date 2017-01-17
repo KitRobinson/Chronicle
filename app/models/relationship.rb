b2a900277c0a38d8d8ef5bcc694309063032369c
Kit Robinson <krobinson.dev@gmail.com>
2017-01-17 13:51:47 -0600

    correct relationship method with missing self flags on variables

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,28 +4,38 @@ class Relationship < ActiveRecord::Base
 	after_create :set_neutral
 
 	def set_neutral
-		current_relationship = 3
+		self.current_relationship = 3
 	end
 
 	def set_expected
-		expected_relationship = find_expected
-		save
+		e = find_expected
+		puts "setting relationship expectation to #{e}"
+		self.expected_relationship = e
+		puts "now my expected relationship = #{self.expected_relationship}"
+		self.save
+		puts "after saving my expected relationship = #{self.expected_relationship}"
 	end
 
 	def adj_current
+		puts "starting relationship = #{self.current_relationship}"
 		2.times do
-			if current_relationship > expected_relationship
-				current_relationship -= 1
-			elsif current_relationship < expected_relationship
-				current_relationship += 1
+			if self.current_relationship > self.expected_relationship
+				self.current_relationship -= 1
+			elsif self.current_relationship < self.expected_relationship
+				self.current_relationship += 1
 			end
+			puts "now my current_relationship is #{self.current_relationship}"
 		end	
-		save
+		self.save
+		puts "final relationship = #{self.current_relationship}"
 	end
 
 	def find_expected
 		a = Attitude.where(organization_id: self.organization_one_id, target_id: self.organization_two_id).first.desired_status
 		b = Attitude.where(organization_id: self.organization_two_id, target_id: self.organization_one_id).first.desired_status
+		puts "attitude a = #{a}"
+		puts "attitude b = #{b}"
+		puts "a new output line!"
 		s = a > b ? a : b
 	end
 
