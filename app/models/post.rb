343cbd4cfe7aa90e1c73ea13fbeabe0de81ebe34
Kit Robinson <krobinson.dev@gmail.com>
2017-01-27 16:48:59 -0600

    create friendlier post list, and recursive date/response count methods for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,20 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :reference, class_name: "Post"
 	has_many :responses, class_name: "Post", foreign_key: :reference_id
+
+	def response_ct_survey
+		rcount = 1
+		responses.each { |resp| rcount += resp.response_ct_survey} 
+		return rcount
+	end
+
+	def response_dt_survey
+		lresp = self.created_at
+		responses.each do |resp| 
+			n = resp.response_dt_survey
+			lresp = lresp > n ? lresp : n
+		end
+		return lresp
+	end
+
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -63,97 +63,24 @@ class Province < ActiveRecord::Base
 
 		#test each Associable, including self if necessary
 		score += self.primary_terrain.doms_score(doms)
-		puts "primary terrain = #{self.primary_terrain.name}, for #{score}"
+		# puts "primary terrain = #{self.primary_terrain.name}, for #{score}"
 		score += self.secondary_terrain.doms_score(doms)
-		puts "secondary terrain = #{self.secondary_terrain.name}, for #{score}"
+		# puts "secondary terrain = #{self.secondary_terrain.name}, for #{score}"
 		self.organizations.each {|org| score += org.doms_score(doms)}
 		self.actors.each {|org| score += org.doms_score(doms)}
-		puts "after orgs and actors: #{score}"
+		# puts "after orgs and actors: #{score}"
 		#then do something with the association score and perception...
 		perception_score = score * deity.actor.perception
-		puts "perception score = score: #{score} * dper: #{deity.actor.perception}"
+		# puts "perception score = score: #{score} * dper: #{deity.actor.perception}"
 		#and then add a flat sagacity bonus
 		visibility = perception_score + (deity.actor.sagacity * 10)
-		puts "sagacity = #{deity.actor.sagacity} * 10"
-		puts "total = #{visibility}"
+		# puts "sagacity = #{deity.actor.sagacity} * 10"
+		# puts "total = #{visibility}"
 		return visibility
 		#principles for deciding the sagacity/perception ratio?
 		#  -  perception applies to much less, so should give about double the bonus.
-		#  -  a perfect domain correlation is 10, so if they are equal, sagacity 3 gives 150, and therefor per 3 should give 300.
+		#  -  a perfect domain correlation is 10, so if they are equal, sagacity 3 gives 30, and therefor per 3 should give 60?
 	end
-
-	def map_report(deity)
-		#depending on the visibility, we pass along a lot or a little information.  At the very least, we pass along a visibility report!
-		vis = visibility(deity)
-		report_data = {}
-		if vis > 0
-			report_data[:primary_terrain]=primary_terrain.name
-		end
-		if vis > 10
-			report_data[:name]=name
-		end
-		if vis > 20
-			report_data[:secondary_terrain]=secondary_terrain.name
-		end
-		if vis > 30
-			report_data[:base_dev]=base_dev
-		end
-		if vis > 40
-			if suzerain
-				report_data[:suzerain] = suzerain.name
-			else
-				report_data[:suzerain] = "none"
-			end 
-		end
-		if vis > 50
-			population_data = {}
-			populations.each do |pop|
-				population_data[pop.race.name] = "present"
-			end
-			report_data[:populations]=population_data
-		end
-		if vis > 60
-			congregation_data = {}
-			congregations.each do |congregation|
-				congregation_data[congregation.deity.name] = "present"
-			end
-			report_data[:congregations]=congregation_data
-		end
-		if vis > 70
-			organization_data = {}
-			organizations.each do |organization|
-				organization_data[organization.name] = "present"
-			end
-			report_data[:organizations] = organization_data
-			populations.each do |pop|
-				report_data[:populations][pop.race.name] = pop.count
-			end
-		end
-		if vis > 80
-			#add infrastructure, congregations details
-		end
-		if vis > 90
-			#add actors, congregation details
-		end
-		if vis > 100 
-			#add artifacts, organization details,
-		end
-		if vis > 110
-			#etc as worked out later
-		end
-		if vis > 120
-			#and more... possibly a bonus on neighbor stuff
-		end
-		if vis < 0
-			report_data[:visibility_score] = 0
-		else
-			report_data[:visibility_score] = vis
-		end
-			#just return that the visibility is 0
-		#datahash will presumably be a json object that we push directly through to the controller?
-		return report_data
-	
-	end		
 
 	def full_report(deity)
 		vis = visibility(deity)
