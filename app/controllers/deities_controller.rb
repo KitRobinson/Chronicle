ebb3c9bc6cc21719b611f4b7628b280054ccde3c
Kit Robinson <krobinson.dev@gmail.com>
2017-03-27 14:29:56 -0400

    add proftext to deity _form for admin edits

--- app/controllers/deities_controller.rb
+++ app/controllers/deities_controller.rb
@@ -81,6 +81,6 @@ class DeitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def deity_params
-      params.require(:deity).permit(:name, :actor_id, :user_id, :description, :profilepic)
+      params.require(:deity).permit(:name, :actor_id, :user_id, :description, :profilepic, :proftext)
     end
 end
