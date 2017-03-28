694b77c5837175a30466a266071408d507af1a72
Kit Robinson <krobinson.dev@gmail.com>
2017-03-28 00:30:51 -0400

    add conversation bar and user tags, configure for pictures, link to conversation show

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,12 @@ class User < ActiveRecord::Base
 	has_many :conversations, through: :userConversations
 
 	def profilepic
-		pic = Deity.where(user: self).first.profilepic
-		pic ? pic : "profile-placeholder.png"
+		if Deity.where(user: self) != []
+			pic = Deity.where(user: self).first.profilepic
+		else
+			pic = "https://chronicles-of-alohmar.s3.amazonaws.com/uploads/1490658660-4-0001-4936/profile-placeholder.png"
+		end
+
 	end
+
 end
