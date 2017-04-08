ac029d7d8bff057df14b0ba3c69972e55ea9c8d6
Kit Robinson <krobinson.dev@gmail.com>
2017-04-08 18:16:46 -0400

    configure mailer and yml files, looks ok

--- app/mailers/update_mailer.rb
+++ app/mailers/update_mailer.rb
@@ -1,2 +1,9 @@
 class UpdateMailer < ApplicationMailer
+	default from: "DoNotReply@chronicle.updates.com"
+
+	def test_email(user)
+		@user = user
+		mail(to: @user.email, subject: "Test Email")
+	end
+	
 end
