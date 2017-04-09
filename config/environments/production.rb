6b6ba055ccaa4cc4430da55da2fad42a06dce811
Kit Robinson <krobinson.dev@gmail.com>
2017-04-08 20:42:37 -0400

    realize the problem was typo smpt -> smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,20 +79,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  require 'tlsmail'    
-  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.perform_deliveries = true
-  ActionMailer::Base.raise_delivery_errors = true
-  ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,  
-    :address            => 'smtp.gmail.com',
-    :port               => 587,
-    :tls                  => true,
-    :domain             => 'gmail.com', #you can also use google.com
-    :authentication     => :plain,
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
     :user_name => ENV['GMAIL_USERNAME'],
     :password => ENV['GMAIL_PASSWORD'],
+    :authentication => 'plain',
+    :enable_starttls_auto => true
   }
+
 end
