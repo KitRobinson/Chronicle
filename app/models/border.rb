d769a774735621bb70bd00ed3356db3499cac659
Kit Robinson <krobinson.dev@gmail.com>
2017-05-06 18:06:07 -0400

    add border seeds, and validations to border model to prevent duplication

--- app/models/border.rb
+++ app/models/border.rb
@@ -1,4 +1,13 @@
 class Border < ActiveRecord::Base
 	belongs_to :province
 	belongs_to :neighbor, class_name: "Province"
+
+	validates :neighbor_id, presence: true
+	validates_uniqueness_of :province_id, :scope => :neighbor_id
+	validate :province_is_primary
+
+	def province_is_primary
+		errors.add(:neighbor_id, "neighbor_id higher than province_id") if neighbor_id > province_id
+	end
+
 end
